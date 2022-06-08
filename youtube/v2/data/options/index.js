5256f2cc9abe61472a3ec7561455339d6441ca29
Hadrien D <hadrien.del@protonmail.com>
2022-06-08 19:41:14 +0200

    [v2] Add possibility to specify user API key and secret

--- youtube/v2/data/options/index.js
+++ youtube/v2/data/options/index.js
@@ -3,51 +3,57 @@
 const toast = document.getElementById('toast');
 
 const restore = () => chrome.storage.local.get({
-  categories: ['Música', 'Music', 'Entertainment'],
-  blacklistAuthors: [],
-  checkCategory: true,
-  manualCheck: false,
-  filter: true,
-  minTime: 30
+    userApiKey: '',
+    userApiSecret: '',
+    categories: ['Música', 'Music', 'Musique', 'Entertainment'],
+    blacklistAuthors: [],
+    checkCategory: true,
+    manualCheck: false,
+    filter: true,
+    minTime: 30
 }, prefs => {
-  document.getElementById('checkCategory').checked = prefs.checkCategory;
-  document.getElementById('filter').checked = prefs.filter;
-  document.getElementById('manualCheck').checked = prefs.manualCheck;
-  document.getElementById('categories').value = prefs.categories.join(', ');
-  document.getElementById('blacklistAuthors').value = prefs.blacklistAuthors.join(', ');
-  document.getElementById('minTime').value = prefs.minTime;
+    document.getElementById('userApiKey').value = prefs.userApiKey;
+    document.getElementById('userApiSecret').value = prefs.userApiSecret;
+    document.getElementById('checkCategory').checked = prefs.checkCategory;
+    document.getElementById('filter').checked = prefs.filter;
+    document.getElementById('manualCheck').checked = prefs.manualCheck;
+    document.getElementById('categories').value = prefs.categories.join(', ');
+    document.getElementById('blacklistAuthors').value = prefs.blacklistAuthors.join(', ');
+    document.getElementById('minTime').value = prefs.minTime;
 });
 restore();
 
-document.getElementById('save').addEventListener('click', () => {
-  chrome.storage.local.set({
-    filter: document.getElementById('filter').checked,
-    checkCategory: document.getElementById('checkCategory').checked,
-    manualCheck: document.getElementById('manualCheck').checked,
-    categories: document.getElementById('categories').value.split(/\s*,\s*/).filter((s, i, l) => s && l.indexOf(s) === i),
-    blacklistAuthors: document.getElementById('blacklistAuthors').value.split(/\s*,\s*/).filter((s, i, l) => s && l.indexOf(s) === i),
-    minTime: +document.getElementById('minTime').value
-  }, () => {
-    toast.textContent = 'Options saved';
-    window.setTimeout(() => toast.textContent = '', 750);
+const resetSession = () => chrome.storage.local.set({ session: null }, () => {
     restore();
-  });
 });
+
+document.getElementById('save').addEventListener('click', () => {
+    chrome.storage.local.set({
+        userApiKey: document.getElementById('userApiKey').value,
+        userApiSecret: document.getElementById('userApiSecret').value,
+        filter: document.getElementById('filter').checked,
+        checkCategory: document.getElementById('checkCategory').checked,
+        manualCheck: document.getElementById('manualCheck').checked,
+        categories: document.getElementById('categories').value.split(/\s*,\s*/).filter((s, i, l) => s && l.indexOf(s) === i),
+        blacklistAuthors: document.getElementById('blacklistAuthors').value.split(/\s*,\s*/).filter((s, i, l) => s && l.indexOf(s) === i),
+        minTime: +document.getElementById('minTime').value
+    }, () => {
+        toast.textContent = 'Options saved';
+        window.setTimeout(() => toast.textContent = '', 750);
+        resetSession();
+    });
+});
+
 // reset
 document.getElementById('reset').addEventListener('click', e => {
-  if (e.detail === 1) {
-    toast.textContent = 'Double-click to reset!';
-    window.setTimeout(() => toast.textContent = '', 750);
-  }
-  else {
-    localStorage.clear();
-    chrome.storage.local.clear(() => {
-      chrome.runtime.reload();
-      window.close();
-    });
-  }
+    if (e.detail === 1) {
+        toast.textContent = 'Double-click to reset!';
+        window.setTimeout(() => toast.textContent = '', 750);
+    } else {
+        localStorage.clear();
+        chrome.storage.local.clear(() => {
+            chrome.runtime.reload();
+            window.close();
+        });
+    }
 });
-// support
-document.getElementById('support').addEventListener('click', () => chrome.tabs.create({
-  url: chrome.runtime.getManifest().homepage_url + '&rd=donate'
-}));
--- youtube/v2/lastfm.js
+++ youtube/v2/lastfm.js
@@ -2,70 +2,86 @@
 'use strict';
 
 var lastfm = {
-  key: window.atob('M2VjNzY1MGEzZmE5MGY3ZGE3YWE5NmFjMzNmMTIzNjc='),
-  secret: window.atob('NzExMzM5YzBjNTQ4ZDJiOWJiMmQ1ZWRmYzg5YzlmY2Q=')
+    // Default key and secret
+    key: window.atob('M2VjNzY1MGEzZmE5MGY3ZGE3YWE5NmFjMzNmMTIzNjc='),
+    secret: window.atob('NzExMzM5YzBjNTQ4ZDJiOWJiMmQ1ZWRmYzg5YzlmY2Q=')
 };
 
+lastfm.init = () => new Promise((resolve, reject) => {
+    chrome.storage.local.get({
+        userApiKey: "",
+        userApiSecret: ""
+    }, onGot => {
+        if (onGot.userApiKey.length > 0) {
+            Object.assign(lastfm, { key: window.atob(onGot.userApiKey) });
+        }
+        if (onGot.userApiSecret.length > 0) {
+            Object.assign(lastfm, { secret: window.atob(onGot.userApiSecret) });
+        }
+        resolve(onGot);
+    });
+});
+
 lastfm.fetch = obj => {
-  obj['api_key'] = lastfm.key;
+    obj['api_key'] = lastfm.key;
 
-  if (obj.method !== 'track.getInfo') {
-    obj['api_sig'] = md5(Object.keys(obj).sort().map(k => k + obj[k]).join('') + lastfm.secret);
-  }
-  obj.format = 'json';
+    if (obj.method !== 'track.getInfo') {
+        obj['api_sig'] = md5(Object.keys(obj).sort().map(k => k + obj[k]).join('') + lastfm.secret);
+    }
+    obj.format = 'json';
 
-  const {sk, ...noSk} = obj;
-  const sentVars = (obj.method !== 'track.getInfo') ? obj : noSk;
+    const { sk, ...noSk } = obj;
+    const sentVars = (obj.method !== 'track.getInfo') ? obj : noSk;
 
-  const url = 'https://ws.audioscrobbler.com/2.0/?' +
-    Object.entries(sentVars).map(([k, v]) => k + '=' + encodeURIComponent(v)).join('&');
+    const url = 'https://ws.audioscrobbler.com/2.0/?' +
+        Object.entries(sentVars).map(([k, v]) => k + '=' + encodeURIComponent(v)).join('&');
 
-  return fetch(url, {
-    method: 'POST'
-  }).then(res => res.json());
+    return fetch(url, {
+        method: 'POST'
+    }).then(res => res.json());
 };
 
 lastfm.authenticate = () => new Promise((resolve, reject) => {
-  const ru = chrome.identity.getRedirectURL('oauth.html');
-  chrome.identity.launchWebAuthFlow({
-    'url': 'https://www.last.fm/api/auth?api_key=' + lastfm.key +
-      '&cb=' + encodeURIComponent(ru) +
-      // a firefox mess!
-      '&redirect_uri=' + encodeURIComponent(ru),
-    'interactive': true
-  }, url => {
-    if (chrome.runtime.lastError || !url) {
-      return reject(chrome.runtime.lastError || 'Empty response from chrome.identity.launchWebAuthFlow');
-    }
-    lastfm.fetch({
-      method: 'auth.getSession',
-      token: url.split('token=').pop().split('&').shift()
-    }).then(json => {
-      if (json && json.error) {
-        reject(json.message);
-      }
-      else if (json && json.session) {
-        chrome.storage.local.set({
-          session: json.session
-        }, resolve);
-      }
-    }).catch(reject);
-  });
+    lastfm.init().then(() => {
+        const ru = chrome.identity.getRedirectURL('oauth.html');
+        chrome.identity.launchWebAuthFlow({
+            'url': 'https://www.last.fm/api/auth?api_key=' + lastfm.key +
+                '&cb=' + encodeURIComponent(ru) +
+                // a firefox mess!
+                '&redirect_uri=' + encodeURIComponent(ru),
+            'interactive': true
+        }, url => {
+            if (chrome.runtime.lastError || !url) {
+                return reject(chrome.runtime.lastError || 'Empty response from chrome.identity.launchWebAuthFlow');
+            }
+            lastfm.fetch({
+                method: 'auth.getSession',
+                token: url.split('token=').pop().split('&').shift()
+            }).then(json => {
+                if (json && json.error) {
+                    reject(json.message);
+                } else if (json && json.session) {
+                    chrome.storage.local.set({
+                        session: json.session
+                    }, resolve);
+                }
+            }).catch(reject);
+        });
+    });
 });
 
 lastfm.call = request => new Promise((resolve, reject) => {
-  chrome.storage.local.get({
-    session: null
-  }, prefs => {
-    if (prefs.session) {
-      lastfm.fetch(Object.assign(request, {
-        sk: prefs.session.key,
-        username: prefs.session.name
-      })).then(resolve, reject);
-    }
-    else {
-      lastfm.authenticate()
-        .then(() => lastfm.call(request).then(resolve, reject), reject);
-    }
-  });
+    chrome.storage.local.get({
+        session: null
+    }, prefs => {
+        if (prefs.session) {
+            lastfm.fetch(Object.assign(request, {
+                sk: prefs.session.key,
+                username: prefs.session.name
+            })).then(resolve, reject);
+        } else {
+            lastfm.authenticate()
+                .then(() => lastfm.call(request).then(resolve, reject), reject);
+        }
+    });
 });
